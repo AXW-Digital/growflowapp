db8ea98ee554ddc01a0eec5847ae2b3ad92c820e
oschku <honkasalo.oskari@gmail.com>
2021-03-04 15:18:30 +0200

    css changes

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -4,53 +4,59 @@ import '../assets/css/index.css'
 class Signin extends Component {
     render() {
         return (
-            <div class = 'container-fluid d-flex vh-100 vw-100 sign-bg m-0 px-0 pb-5'>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-                        <div class="card card-signin my-5">
-                            <div class="card-body">
-                                <h5 class="card-title text-center">Kirjaudu</h5>
-                                <form class="form-signin">
-                                    <div class="form-label-group">
-                                        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus />
-                                        <label for="inputEmail">Sähköposti</label>
+            <div className='container-fluid d-flex vh-100 vw-100 sign-bg m-0 px-0 pb-5'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                            <div className="card card-signin my-5">
+                                <div className="card-body">
+                                    <div class="d-flex flex-row-reverse">
+                                    <a 
+                                    href="/">
+                                    <i className="bx bx-exit bx-sm bx-sign"></i>
+                                    </a>
                                     </div>
-
-                                    <div class="form-label-group">
-                                        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required />
-                                        <label for="inputPassword">Salasana</label>
-                                    </div>
-
-                                    <div class="custom-control custom-checkbox mb-3">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1" />
-                                        <label class="custom-control-label" for="customCheck1">Muista minut</label>
-                                    </div>
-                                    <button class="btn btn-lg btn-block text-uppercase btn-login" type="submit">Kirjaudu</button>
-                                    <div class='row mt-4 mb-3'>
-                                        <div class="col"><hr /></div>
-                                        <div class='col align-items-center align-middle my-auto text-nowrap alt-sign text-center'>tai kirjaudu</div>
-                                        <div class="col"><hr /></div>
-                                    </div>
-                                    <div class='row justify-content-between ml-xl-4 mr-xl-4 ml-0 mr-0'>
-                                        <a href="#" class="fab fa-facebook-f btn-some align-items-center justify-content-center"></a>
-                                        <a href="#" class="fab fa-google btn-some align-items-center justify-content-center"></a>
-                                        <a href="#" class="fab fa-linkedin btn-some align-items-center justify-content-center"></a>
-                                        <a href="#" class="fab fa-twitter btn-some align-items-center justify-content-center"></a>
-                                    </div>
-                                    <hr class = 'my-4'/>
-                                    <div class='row mt-1'>
-                                        <div class='col text-center text-nowrap'>Ei käyttäjätunnusta?</div>
-                                        <div class='col text-center'>
-                                            <a href='/signup' class='link-primary'>Rekisteröidy</a>
+                                    <h5 className="card-title text-center">Kirjaudu</h5>
+                                    <form className="form-signin">
+                                        <div className="form-label-group">
+                                            <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+                                            <label for="inputEmail">Sähköposti</label>
                                         </div>
-                                    </div>
-                                </form>
+
+                                        <div className="form-label-group">
+                                            <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+                                            <label for="inputPassword">Salasana</label>
+                                        </div>
+
+                                        <div className="custom-control custom-checkbox mb-3">
+                                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                                            <label className="custom-control-label" for="customCheck1">Muista minut</label>
+                                        </div>
+                                        <button className="btn btn-lg btn-block text-uppercase btn-login" type="submit">Kirjaudu</button>
+                                        <div className='row mt-4 mb-3'>
+                                            <div className="col"><hr /></div>
+                                            <div className='col align-items-center align-middle my-auto text-nowrap alt-sign text-center'>tai kirjaudu</div>
+                                            <div className="col"><hr /></div>
+                                        </div>
+                                        <div className='row justify-content-between ml-xl-4 mr-xl-4 ml-0 mr-0'>
+                                            <a href="#" className="fab fa-facebook-f btn-some align-items-center justify-content-center"></a>
+                                            <a href="#" className="fab fa-google btn-some align-items-center justify-content-center"></a>
+                                            <a href="#" className="fab fa-linkedin btn-some align-items-center justify-content-center"></a>
+                                            <a href="#" className="fab fa-twitter btn-some align-items-center justify-content-center"></a>
+                                        </div>
+                                        <hr className='my-4' />
+                                        <div className='row mt-1'>
+                                            <div className='col text-center text-nowrap'>Ei käyttäjätunnusta?</div>
+                                            <div className='col text-center'>
+                                                <a href='/signup' className='link-primary'>Rekisteröidy</a>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
             </div>
 
         );
