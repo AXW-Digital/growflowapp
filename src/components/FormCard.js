2ebe95fc55159548f2761aaf5a4dd1088228593f
oschku <oschku@users.noreply.github.com>
2021-03-07 20:31:30 +0200

    Added a mapping and filter function to add cards

--- src/components/FormCard.js
+++ src/components/FormCard.js
@@ -15,24 +15,24 @@ export default class FormCard extends Component {
         return (
             <div className="col-xl-4 col-md-6 my-3 kysely-col" data-aos="fade-up" data-aos-delay="200">
 
-                <div className={`kysely-card ${this.props.kysely.color}`}>
+                <div className={`kysely-card ${this.props.color}`}>
                     <div className='container-fluid'>
                         <div className='row row-image'>
 
                                 <img className="card-image"
-                                    src={this.props.kysely.picUrl}
-                                    alt={this.props.kysely.name}
+                                    src={this.props.picUrl}
+                                    alt={this.props.name}
                                 />
 
                         </div>
                         <div className='row row-title'>
-                            <h3 className="card-title">{this.props.kysely.formTitle}</h3>
+                            <h3 className="card-title">{this.props.formTitle}</h3>
                         </div>
                         <div className='row row-text'>
-                            <p className="card-text">{this.props.kysely.formText}</p>
+                            <p className="card-text">{this.props.formText}</p>
                         </div>
                         <div className='row row-button'>
-                            <a href={this.props.kysely.formUrl} className="btn btn-primary btn-card my-auto text-center">{this.props.tyyppi} {this.props.kysely.minutes}</a>
+                            <a href={this.props.formUrl} className="btn btn-primary btn-card my-auto text-center">{this.props.tyyppi} {this.props.minutes}</a>
                         </div>
                     </div>
                 </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,51 +3,25 @@ import Header from '../components/HeaderHome';
 import Footer from '../components/Footer'
 import FormCard from '../components/FormCard'
 import { ReactComponent as Ideas } from '../assets/images/ideas.svg'
+import cardvaluelist from '../assets/js/cardvalues'
 var Name = 'John Doe'
 
-
-const mcdonalds_1 = {
-    name: "McDonald's", picUrl: '/img/mcd.jfif', formTitle: 'McFeedback™',
-    formText: 'Miten meillä meni? Anna palautetta viimeisimmästä ravintolakäynnistäsi ja auta meitä kehittämään toimintaamme.',
-    formUrl: '#', color:'blue', minutes:'(3 min)'
+// Function that creates the card content for the mapping function
+function createCard(cardContent){
+    return (
+        <FormCard
+        key = {cardContent.id}
+        name = {cardContent.name}
+        picUrl = {cardContent.picUrl}
+        formTitle = {cardContent.formTitle}
+        formText = {cardContent.formText}
+        formUrl = {cardContent.formUrl}
+        color = {cardContent.color}
+        minutes = {cardContent.minutes}
+        tyyppi = {cardContent.tyyppi}
+        />
+    )
 }
-const mcdonalds_2 = {
-    name: "McDonald's", picUrl: '/img/beef.jfif', formTitle: 'Menutestaus Helsinki',
-    formText: 'Uusi French Beef. Valkosipulisämpylän välissä kaksi 100% naudanlihapihviä, siivu emmental-sulatejuustoa, kaksi viipaletta pekonia ja bataviasalaattia.',
-    formUrl: '#', color:'purple'
-}
-const mcdonalds_3 = {
-    name: "McDonald's", picUrl: '/img/salaatti.jfif', formTitle: 'All Vegan -salaatti',
-    formText: 'Uudessa raikkaassa All Vegan -salaatissa maistuvat soijapavut, lehtikaali, pinaatti ja pikkelöity porkkana. Nimensä mukaisesti salaatti on 100 % vegaaninen.',
-    formUrl: '#', color:'purple'
-}
-
-const hese_1 = {
-    name: "Hesburger", picUrl: '/img/hese.jpg', formTitle: 'Kiitä, moiti, kysy tai ehdota',
-    formText: 'Saitko Hesburgerissa erityisen mukavaa palvelua? Huomasitko jossain parantamisen varaa? Haluaisitko valikoimiimme uusia tuotteita? Kaikki palaute on meille arvokasta. ',
-    formUrl: '#', color:'green', minutes:'(5 min)'
-}
-
-const hese_2 = {
-    name: "Hesburger", picUrl: '/img/hese2.png', formTitle: 'Menutestaus Jyväskylä',
-    formText: 'Supersuosittu Kebab-hampurilainen.',
-    formUrl: '#', color:'green'
-}
-
-
-const burgerking_1 = {
-    name: "Burger King", picUrl: '/img/burgerking.jpeg', formTitle: 'Oma BK-Kokemukseni',
-    formText: 'Tervetuloa Minun BURGER KING® -kokemukseni -kyselyyn. Arvostamme rehellistä palautettasi ja sitä, että käytät aikaasi kyselyyn vastaamiseen.',
-    formUrl: '#', color:'red', minutes:'(10 min)'
-}
-
-const burgerking_2 = {
-    name: "Burger King", picUrl: '/img/whopper.png', formTitle: 'Menutestaus Tampere',
-    formText: 'TRIPLA WHOPPER - Kolme kerrosta makusi mukaan',
-    formUrl: '#', color:'blue'
-}
-
-
 
 
 export default class HomePage extends Component {
@@ -78,12 +52,7 @@ export default class HomePage extends Component {
                             <p>Veritatis et dolores facere numquam et praesentium</p>
                         </header>
                         <div class="row g-4 d-flex">
-                        <FormCard kysely={mcdonalds_1} tyyppi='Vastaa' />
-                        <FormCard kysely={hese_1} tyyppi='Vastaa' />
-                        <FormCard kysely={burgerking_1} tyyppi='Vastaa' />
-                        <FormCard kysely={hese_1} tyyppi='Vastaa' />
-                        <FormCard kysely={mcdonalds_1} tyyppi='Vastaa' />
-                        <FormCard kysely={burgerking_1} tyyppi='Vastaa' />
+                            {cardvaluelist.filter(card => card.tyyppi === 'Vastaa').map(createCard)}
                         </div>
                     </div>
                 </section>
@@ -94,11 +63,7 @@ export default class HomePage extends Component {
                             <p>Veritatis et dolores facere numquam et praesentium</p>
                         </header>
                         <div class="row g-4 d-flex">
-                        <FormCard kysely={mcdonalds_2} tyyppi='Osallistu' />
-                        <FormCard kysely={hese_2} tyyppi='Osallistu' />
-                        <FormCard kysely={burgerking_2} tyyppi='Osallistu' />
-                        <FormCard kysely={mcdonalds_3} tyyppi='Osallistu' />
-                        <FormCard kysely={burgerking_2} tyyppi='Osallistu' />
+                        {cardvaluelist.filter(card => card.tyyppi === 'Osallistu').map(createCard)}
                         </div>
                     </div>
                 </section>
