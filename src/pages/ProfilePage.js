2713ea55d6df8c6f418a9437c19bdb390d714de7
oschku <honkasalo.oskari@gmail.com>
2021-03-09 11:00:48 +0200

    added profilepage

---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HeaderHome from '../components/HeaderHome'
+
+class ProfilePage extends Component {
+    render() {
+        return (
+            <div>
+                <HeaderHome />
+            </div>
+        );
+    }
+}
+
+export default ProfilePage;
